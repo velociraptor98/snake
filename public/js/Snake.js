6f8997cebae98921ca2451c7fca2f6babb358feb
kunal <kunal1998doon@gmail.com>
2019-09-28 19:48:43 +0530

    Added objective
    objective object added.
    ensuring everything is placed in a 16*16 grid.

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -6,9 +6,20 @@ export default class Snake{
         this.moveInterval=450;//1000 milliseconds == 1 second
         this.direction=Phaser.Math.Vector2.RIGHT;
         this.body=[];
-        this.body.push(this.scene.add.rectangle(0,0,16,16,0xffffff).setOrigin(0));
+        this.SnakeSize=16;
+        this.body.push(this.scene.add.rectangle(this.scene.game.config.width/2,this.scene.game.config.height/2,this.SnakeSize,this.SnakeSize,0xffffff).setOrigin(0));
+        this.apple=this.scene.add.rectangle(100,100,this.SnakeSize,this.SnakeSize,0x0000ff).setOrigin(0);
+        this.RandomApple();
         this.scene.input.keyboard.on('keydown',e => {this.keydown(e);});
     }
+
+    RandomApple()
+    {
+        //ensure to keep it a multiple of 16 to ensure snake can actually reach it
+        this.apple.x=Math.floor(Math.random()*this.scene.game.config.width/this.SnakeSize)*this.SnakeSize;
+        this.apple.y=Math.floor(Math.random()*this.scene.game.config.height/this.SnakeSize)*this.SnakeSize;
+    }
+
     keydown(event)
     {
         switch(event.keyCode)
@@ -43,7 +54,12 @@ export default class Snake{
 
     draw()
     {
-        this.body[0].x+=this.direction.x*16 ;
-        this.body[0].y+=this.direction.y*16;
+        for(let index=this.body.length-1;index>0;--index)
+        {
+            this.body[index].x=this.body[index-1].x;
+            this.body[index].y=this.body[index-1].y;
+        }
+        this.body[0].x+=this.direction.x*this.SnakeSize;
+        this.body[0].y+=this.direction.y*this.SnakeSize;
     }
 }
